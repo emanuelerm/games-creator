af78be1b9e72dcf6040ed7f3b031a6734a0c8001
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-06-06 19:07:21 +0200

    fix chri face

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
 @vite('resources/scss/animation/carousel.scss')
-<main class="d-flex align-items-center">
+<main class="d-flex align-items-center my-3">
     <div class="container scroll d-flex gap-5">
         @foreach ($characters as $character)
-            <div class="card">
-                <h3>{{ $character->name }}</h3>
+            <div class="card text-white d-flex flex-column align-items-center justify-content-between p-2 text-center">
+                <div class="card-title">
+                    <h5 class="m-0">{{ $character->name }}</h5>
+                </div>
+                <div>
+                    <img src="/image/chri.jpg" alt="{{$character->name }}">
+                </div>
             </div>
+            
         @endforeach
     </div>
 </main>
