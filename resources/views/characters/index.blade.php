b0eefde145d6ccd1a6d03c508fe4a7e331466b2e
emanuelerm <emanueler00@gmail.com>
2023-06-06 18:18:34 +0200

    fix

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+@vite('resources/scss/animation/carousel.scss')
 <main class="d-flex align-items-center">
     <div class="container scroll d-flex gap-5">
         @foreach ($characters as $character)
