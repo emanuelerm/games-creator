cdbb03adf5379f6f79d2950260ba7ad1a2585fdc
emanuelerm <emanueler00@gmail.com>
2023-06-07 13:40:23 +0200

    fix text overflow

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -7,13 +7,13 @@
         @foreach ($characters as $character)
             <div class="card text-white d-flex flex-column align-items-center justify-content-between p-2 text-center">
                 <div class="card-title">
-                    <h5 class="m-0">{{ $character->name }}</h5>
+                    <h5 class="m-0 truncate">{{ $character->name }}</h5>
                 </div>
                 <div>
                     <img src="/image/chri.jpg" alt="{{$character->name }}">
                 </div>
             </div>
-            
+
         @endforeach
     </div>
 </main>
