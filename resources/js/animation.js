df9b5084efac22caba8687ce4eb70a4452c24b8f
emanuelerm <emanueler00@gmail.com>
2023-06-15 17:08:45 +0200

    transition

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -1,26 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var cards = document.querySelectorAll(".card");
+    let cards = document.querySelectorAll(".card");
+    const MARGIN = 10; // Margine in pixel intorno alla carta
+
+    function handleMouseEnter() {
+        this.style.transition = "box-shadow 0.55s ease";
+        this.style.boxShadow = "0px 1px 20px 1px #ef3b2d";
+    }
+
+    function handleMouseLeave() {
+        this.style.transition = "box-shadow 0.55s ease";
+        this.style.boxShadow = "none";
+    }
 
     function handleMouseMove(e) {
-        var rect = this.getBoundingClientRect();
-        var cardCenterX = rect.left + rect.width / 2;
-        var cardCenterY = rect.top + rect.height / 2;
-        var xAxis = (cardCenterX - e.clientX) / 10;
-        var yAxis = (cardCenterY - e.clientY) / 10;
-        var scale = 1;
+        let rect = this.getBoundingClientRect();
+        let cardCenterX = rect.left + rect.width / 2;
+        let cardCenterY = rect.top + rect.height / 2;
+        let xAxis = (cardCenterX - e.clientX) / 10;
+        let yAxis = (cardCenterY - e.clientY) / 10;
+        let scale = 1;
 
-        this.style.transform =
-            "perspective(1000px) rotateY(" +
-            xAxis +
-            "deg) rotateX(" +
-            yAxis +
-            "deg) scale(" +
-            scale +
-            ")";
+        // Controllo se il cursore si trova nel margine intorno alla carta
+        if (
+            e.clientX < rect.left + MARGIN ||
+            e.clientX > rect.right - MARGIN ||
+            e.clientY < rect.top + MARGIN ||
+            e.clientY > rect.bottom - MARGIN
+        ) {
+            // Il cursore si trova nel margine, non applicare la trasformazione
+            this.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
+        } else {
+            // Il cursore è all'interno della carta, applica la trasformazione
+            this.style.transform =
+                "perspective(1000px) rotateY(" +
+                xAxis +
+                "deg) rotateX(" +
+                yAxis +
+                "deg) scale(" +
+                scale +
+                ")";
+        }
     }
 
     function resetCard() {
-        this.style.transition = "transform 0.3s ease";
+        this.style.transition = "box-shadow 0.55s ease";
         this.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
     }
 
@@ -33,13 +56,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             });
 
-            card.style.transition = "none";
             card.addEventListener("mousemove", handleMouseMove);
+            handleMouseEnter.call(card);
         });
 
         card.addEventListener("mouseleave", function () {
             card.removeEventListener("mousemove", handleMouseMove);
             resetCard.call(card);
+            handleMouseLeave.call(card);
         });
     });
 });
