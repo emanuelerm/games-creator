714eed8a44a55ce3e77841a556ba3ec821b3eb3e
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-06-15 18:44:11 +0200

    ad fix

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -1,39 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-
-<form action="{{ route('admin.characters.store') }}" method="POST">
-    @csrf
-    <div class="row mb-3 mt-5 mx-5 text-white">
-        <div class="col-12 mb-5">
-            <label for="name" class="form-label">Nome</label>
-            <input type="text" name="name" class="form-control" id="name">
-        </div>
-        <div class="col-12 mb-5">
-            <label for="description">Descrizione</label>
-            <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
-        </div>
-        <div class="col-3">
-            <label for="attack">Attacco</label>
-            <input type="number" name="attack" id="attack" class="form-control">
-        </div>
-        <div class="col-3">
-            <label for="defence">Difesa</label>
-            <input type="number" name="defence" id="defence" class="form-control">
-        </div>
-        <div class="col-3">
-            <label for="speed">Velocità</label>
-            <input type="number" name="speed" id="speed" class="form-control">
-        </div>
-        <div class="col-3">
-            <label for="life">Vita</label>
-            <input type="number" name="life" id="life" class="form-control">
-        </div>
-        <div class="d-flex justify-content-center mb-3 gap-3 mt-5">
-            <button type="submit" class="btn btn-primary">Add character</button>
-            <button type="reset" class="btn btn-secondary px-4">Reset form</button>
-        </div>
-    </div>
-</form>
-
+	<form action="{{ route('admin.characters.store') }}" method="POST">
+		@csrf
+		<div class="row mb-3 mt-5 mx-5 text-white">
+			<div class="col-12 mb-5">
+				<label for="name" class="form-label">Nome</label>
+				<input type="text" name="name" class="form-control" id="name">
+			</div>
+			<div class="col-12 mb-5">
+				<label for="description">Descrizione</label>
+				<textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
+			</div>
+			<div class="col-3">
+				<label for="attack">Attacco</label>
+				<input type="number" name="attack" id="attack" class="form-control">
+			</div>
+			<div class="col-3">
+				<label for="defence">Difesa</label>
+				<input type="number" name="defence" id="defence" class="form-control">
+			</div>
+			<div class="col-3">
+				<label for="speed">Velocità</label>
+				<input type="number" name="speed" id="speed" class="form-control">
+			</div>
+			<div class="col-3">
+				<label for="life">Vita</label>
+				<input type="number" name="life" id="life" class="form-control">
+			</div>
+			<div class="d-flex justify-content-center mb-3 gap-3 mt-5">
+				<button type="submit" class="btn btn-primary">Add character</button>
+				<button type="reset" class="btn btn-secondary px-4">Reset form</button>
+			</div>
+		</div>
+	</form>
 @endsection
